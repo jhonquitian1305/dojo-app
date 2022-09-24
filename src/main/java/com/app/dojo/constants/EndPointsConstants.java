733881b9db07c85d0293d7ddf1da4e4a81ff7a17
Andrés Castro <andres.cmonsalve@gmail.com>
2022-09-24 10:59:05 -0500

    Feat: Create EndPointsConstantas where the routes to be used in controllers are defined

---
+++ src/main/java/com/app/dojo/constants/EndPointsConstants.java
@@ -0,0 +1,11 @@
+package com.app.dojo.constants;
+
+public final class EndPointsConstants {
+
+    ///// General
+    public static final String ENDPOINT_ID = "/{id}";
+
+    // Rooms
+    public static final String ENDPOINT_ROOMS="/api/dojo-app/rooms";
+
+}
