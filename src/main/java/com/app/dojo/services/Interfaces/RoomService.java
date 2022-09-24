14cfa10c8acc419f73694022d4078fd6d91386ab
Andrés Castro <andres.cmonsalve@gmail.com>
2022-09-24 10:51:33 -0500

    Feat: create interface to implement service for room model

---
+++ src/main/java/com/app/dojo/services/Interfaces/RoomService.java
@@ -0,0 +1,8 @@
+package com.app.dojo.services.Interfaces;
+
+import com.app.dojo.dtos.RoomDTO;
+import com.app.dojo.models.Room;
+
+public interface RoomService {
+    public RoomDTO create(RoomDTO roomDTO);
+}
