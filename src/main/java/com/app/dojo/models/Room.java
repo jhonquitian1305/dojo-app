82b0692dbf4325f45ddc5ba105051888da9b9c9b
Andrés Castro <andres.cmonsalve@gmail.com>
2022-09-24 14:14:04 -0500

    Feat:Add unique constraint to  roomName column in room model

--- src/main/java/com/app/dojo/models/Room.java
+++ src/main/java/com/app/dojo/models/Room.java
@@ -8,7 +8,7 @@ public class Room {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true)
     private String roomName;
 
     public Room() {
