964a4333f5af6a0879816ca915f27fe04ce75d16
Andrés Castro <andres.cmonsalve@gmail.com>
2022-09-24 14:46:51 -0500

    Fix:Add folder to group the  Exception classes

--- src/main/java/com/app/dojo/exception/ResponseExceptionHandler.java
+++ src/main/java/com/app/dojo/exception/ResponseExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.app.dojo.exception;
 
+import com.app.dojo.exception.errors.BadRequest;
+import com.app.dojo.exception.errors.NotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -23,5 +25,11 @@ public class ResponseExceptionHandler extends ResponseEntityExceptionHandler {
     public final ResponseEntity<ExceptionResponse> handlerBadRequestException(BadRequest badRequest, WebRequest webRequest){
         ExceptionResponse exceptionResponse= new ExceptionResponse(LocalDateTime.now(), badRequest.getMessage(),webRequest.getDescription(false));
         return  new ResponseEntity<>(exceptionResponse,HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(NotFoundException.class)
+    public final ResponseEntity<ExceptionResponse> handlerNotFoundException(NotFoundException notFoundException, WebRequest request){
+        ExceptionResponse  exceptionResponse= new ExceptionResponse(LocalDateTime.now(), notFoundException.getMessage(), request.getDescription(false));
+        return  new ResponseEntity<>(exceptionResponse, HttpStatus.NOT_FOUND);
     }
 }
--- src/main/java/com/app/dojo/exception/errors/BadRequest.java
+++ src/main/java/com/app/dojo/exception/errors/BadRequest.java
@@ -1,4 +1,4 @@
-package com.app.dojo.exception;
+package com.app.dojo.exception.errors;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
