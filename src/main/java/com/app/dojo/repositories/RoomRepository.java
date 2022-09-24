bc422400c9d546e186d598763f046f7ea6ee13bd
Andrés Castro <andres.cmonsalve@gmail.com>
2022-09-24 14:26:07 -0500

    Feat: Find all rooms

--- src/main/java/com/app/dojo/repositories/RoomRepository.java
+++ src/main/java/com/app/dojo/repositories/RoomRepository.java
@@ -5,10 +5,12 @@ import com.app.dojo.models.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface RoomRepository extends JpaRepository<Room,Long> {
     public Optional<Room> findByRoomName(String roomName);
     public Optional<Room> findById(Long id);
+    public List<Room> findAll();
 }
