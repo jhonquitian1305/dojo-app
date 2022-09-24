00e9bea71f1b48e64f77797019444ee2b4d332b6
Andrés Castro <andres.cmonsalve@gmail.com>
2022-09-24 10:48:20 -0500

    Feat: Create repository for Room model

---
+++ src/main/java/com/app/dojo/repositories/RoomRepository.java
@@ -0,0 +1,10 @@
+package com.app.dojo.repositories;
+
+
+import com.app.dojo.models.Room;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoomRepository extends JpaRepository<Room,Long> {
+}
