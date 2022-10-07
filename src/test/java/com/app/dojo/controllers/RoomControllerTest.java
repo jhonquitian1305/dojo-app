c110685681a6978eddf52264fa3c6c0ed703cc31
Andrés Castro <andres.cmonsalve@gmail.com>
2022-10-06 20:59:55 -0500

    Feat: Create RoomControllerTest to make test to RoomController

---
+++ src/test/java/com/app/dojo/controllers/RoomControllerTest.java
@@ -0,0 +1,10 @@
+package com.app.dojo.controllers;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@WebMvcTest
+class RoomControllerTest {
+
+}
