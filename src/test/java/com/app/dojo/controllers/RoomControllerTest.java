6f9e837b1909839f4b82e500918415e50bc14ed0
Andrés Castro <andres.cmonsalve@gmail.com>
2022-10-06 21:42:38 -0500

    Feat: Test Controller, create a room and try to create a room that already be created

--- src/test/java/com/app/dojo/controllers/RoomControllerTest.java
+++ src/test/java/com/app/dojo/controllers/RoomControllerTest.java
@@ -1,10 +1,48 @@
 package com.app.dojo.controllers;
 
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import com.app.dojo.builders.builderDTO.RoomDTOBuilder;
+import com.app.dojo.dtos.RoomDTO;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@WebMvcTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles(profiles = "test")
 class RoomControllerTest {
 
+    @Autowired
+    private TestRestTemplate testRestTemplate;
+    private RoomDTO roomDTO;
+    private String url="http://localhost:8080/api/dojo-app/rooms";
+
+    @BeforeEach
+    void init(){
+        roomDTO= new RoomDTOBuilder()
+                .setRoomName("202")
+                .setId(1L)
+                .build();
+    }
+
+    @Order(1)
+    @Test
+    @DisplayName("Test Controller, create a room")
+    void create() throws Exception {
+        ResponseEntity<RoomDTO> response=testRestTemplate.postForEntity(url,roomDTO,RoomDTO.class);
+        assertEquals(201,response.getStatusCodeValue());
+        assertEquals(MediaType.APPLICATION_JSON,response.getHeaders().getContentType());
+        assertNotNull(response.getBody());
+    }
+    @Order(2)
+    @Test
+    void failCreate(){
+        ResponseEntity<RoomDTO> response=testRestTemplate.postForEntity(url,roomDTO,RoomDTO.class);
+        assertEquals(400,response.getStatusCodeValue());
+    }
 }
