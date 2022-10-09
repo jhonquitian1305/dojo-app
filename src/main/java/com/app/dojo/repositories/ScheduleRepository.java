0bbac18072b34af119b496f707d24be7b9979d61
Andrés Castro <andres.cmonsalve@gmail.com>
2022-10-09 15:38:54 -0500

    Feat: Create a method to determinate whether there is a schedule recorded with a day and time

--- src/main/java/com/app/dojo/repositories/ScheduleRepository.java
+++ src/main/java/com/app/dojo/repositories/ScheduleRepository.java
@@ -1,9 +1,12 @@
 package com.app.dojo.repositories;
 
+import com.app.dojo.models.Day;
+import com.app.dojo.models.Hour;
 import com.app.dojo.models.Schedule;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ScheduleRepository extends JpaRepository<Schedule,Long> {
+    boolean existsScheduleByDaysAndHours(Day day, Hour hour);
 }
