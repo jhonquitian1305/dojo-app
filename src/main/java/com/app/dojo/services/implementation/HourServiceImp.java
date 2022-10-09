1e7e59d3f7d4ff3fda299d5c88c561ee486c2ba1
Andrés Castro <andres.cmonsalve@gmail.com>
2022-10-09 15:15:21 -0500

    Feat: Implemented function  to find a hour by name

--- src/main/java/com/app/dojo/services/implementation/HourServiceImp.java
+++ src/main/java/com/app/dojo/services/implementation/HourServiceImp.java
@@ -2,14 +2,22 @@ package com.app.dojo.services.implementation;
 
 import com.app.dojo.exception.errors.NotFoundException;
 import com.app.dojo.models.Hour;
+import com.app.dojo.repositories.HourRepository;
 import com.app.dojo.services.Interfaces.HourService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class HourServiceImp implements HourService {
 
+    @Autowired
+    private HourRepository hourRepository;
     @Override
     public Hour findByName(String hour) throws NotFoundException {
-        return null;
+        Hour hourFound=this.hourRepository.findByHour(hour);
+        if(hourFound==null){
+            throw  new NotFoundException("Hour doesn' exist");
+        }
+        return  hourFound;
     }
 }
