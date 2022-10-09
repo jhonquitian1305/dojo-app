59ae7891e423cbde80d49f7bf5cfaf238ae9411c
Andrés Castro <andres.cmonsalve@gmail.com>
2022-10-09 15:09:52 -0500

    Feat:Implemented function  to find a day by name

--- src/main/java/com/app/dojo/services/implementation/DayServiceImp.java
+++ src/main/java/com/app/dojo/services/implementation/DayServiceImp.java
@@ -2,13 +2,26 @@ package com.app.dojo.services.implementation;
 
 import com.app.dojo.exception.errors.NotFoundException;
 import com.app.dojo.models.Day;
+import com.app.dojo.repositories.DayRespository;
 import com.app.dojo.services.Interfaces.DayServcie;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.swing.text.html.Option;
+import java.util.Optional;
 
 @Service
 public class DayServiceImp implements DayServcie {
+
+    @Autowired
+    private DayRespository dayRespository;
+
     @Override
     public Day findByName(String day) throws NotFoundException {
-        return null;
+       Day dayFound=this.dayRespository.findByDay(day);
+       if(dayFound==null){
+           throw  new NotFoundException("Day doesn't exist");
+       }
+       return  dayFound;
     }
 }
