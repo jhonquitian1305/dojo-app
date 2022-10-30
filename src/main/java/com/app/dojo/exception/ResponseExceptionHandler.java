56324d1e9dba5f47daf143c716c1da90efe0c613
Andrés Castro <andres.cmonsalve@gmail.com>
2022-10-29 20:43:13 -0500

    Feat:Method for handling validator excpetions  in DTOs

--- src/main/java/com/app/dojo/exception/ResponseExceptionHandler.java
+++ src/main/java/com/app/dojo/exception/ResponseExceptionHandler.java
@@ -2,14 +2,20 @@ package com.app.dojo.exception;
 
 import com.app.dojo.exception.errors.BadRequest;
 import com.app.dojo.exception.errors.NotFoundException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.lang.reflect.Field;
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class ResponseExceptionHandler extends ResponseEntityExceptionHandler {
@@ -31,5 +37,17 @@ public class ResponseExceptionHandler extends ResponseEntityExceptionHandler {
     public final ResponseEntity<ExceptionResponse> handlerNotFoundException(NotFoundException notFoundException, WebRequest request){
         ExceptionResponse  exceptionResponse= new ExceptionResponse(LocalDateTime.now(), notFoundException.getMessage(), request.getDescription(false));
         return  new ResponseEntity<>(exceptionResponse, HttpStatus.NOT_FOUND);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request){
+        Map<String,String> errors= new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach(error->{
+            String fieldName= ((FieldError) error).getField();
+            String message=error.getDefaultMessage();
+            errors.put(fieldName,message);
+        });
+
+        return new ResponseEntity<Object>(errors,HttpStatus.BAD_REQUEST);
     }
 }
