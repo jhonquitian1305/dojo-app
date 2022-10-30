36bd1015cfeede3136117932409149df8ff483d8
Andrés Castro <andres.cmonsalve@gmail.com>
2022-10-29 22:49:47 -0500

    Feat: test to find all schedules

--- src/test/java/com/app/dojo/repositories/ScheduleRepositoryTest.java
+++ src/test/java/com/app/dojo/repositories/ScheduleRepositoryTest.java
@@ -4,11 +4,15 @@ import com.app.dojo.builders.builderModels.ScheduleBuilder;
 import com.app.dojo.models.Schedule;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
 
+import java.util.List;
+
+import static org.assertj.core.api.InstanceOfAssertFactories.LIST;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -31,11 +35,24 @@ class ScheduleRepositoryTest {
     }
 
     @Test
+    @DisplayName("Test Schedule Repository create a schedule")
     void create(){
         Schedule scheduleSaved=this.scheduleRepository.save(schedule);
 
         assertNotNull(scheduleSaved);
         assertThat(scheduleSaved.getId()).isGreaterThan(0L);
+    }
+
+    @Test
+    @DisplayName("Test Schedule Repository find all schedules")
+    void findAll(){
+        // given
+            this.scheduleRepository.save(schedule);
+        // when
+            List<Schedule> allSchedules=this.scheduleRepository.findAll();
+        // then
+            assertThat(allSchedules.size()).isGreaterThan(0);
+            assertNotNull(allSchedules);
     }
 
 }
