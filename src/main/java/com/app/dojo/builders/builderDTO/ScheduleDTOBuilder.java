5d5b24a9031e9e6f24e44dfbcaf82dd3b0ecd66d
Andrés Castro <andres.cmonsalve@gmail.com>
2022-10-29 20:33:53 -0500

    Fix:ScheduleDTOBuilder file modified by changes made in the ScheduleDTO file

--- src/main/java/com/app/dojo/builders/builderDTO/ScheduleDTOBuilder.java
+++ src/main/java/com/app/dojo/builders/builderDTO/ScheduleDTOBuilder.java
@@ -1,33 +1,30 @@
 package com.app.dojo.builders.builderDTO;
 
 import com.app.dojo.dtos.ScheduleDTO;
-import com.app.dojo.models.Day;
-import com.app.dojo.models.Hour;
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class ScheduleDTOBuilder {
     private Long id;
-    private List<Hour> hours;
-    private List<Day> days;
+    private String dayName;
+    private String hoursClass;
 
     public ScheduleDTOBuilder setId(Long id) {
         this.id = id;
         return this;
     }
 
-    public ScheduleDTOBuilder setHours(List<Hour> hours) {
-        this.hours = hours;
+    public ScheduleDTOBuilder setDayName(String dayName) {
+        this.dayName = dayName;
         return this;
     }
 
-    public ScheduleDTOBuilder setDays(List<Day> days) {
-        this.days = days;
+    public ScheduleDTOBuilder setHoursClass(String hoursClass) {
+        this.hoursClass = hoursClass;
         return this;
     }
 
     public ScheduleDTO build(){
-        return  new ScheduleDTO(id,hours,days);
+        return  new ScheduleDTO(id,dayName,hoursClass);
     }
 }
