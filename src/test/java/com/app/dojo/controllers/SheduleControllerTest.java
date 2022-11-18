48ae7958f7effcc611e66bcb44aa68c692e06d53
Andrés Castro <andres.cmonsalve@gmail.com>
2022-11-18 11:03:17 -0500

    Feat: Test to create a schedule in SheduleControllerTest class

--- src/test/java/com/app/dojo/controllers/SheduleControllerTest.java
+++ src/test/java/com/app/dojo/controllers/SheduleControllerTest.java
@@ -1,12 +1,15 @@
 package com.app.dojo.controllers;
 
+import com.app.dojo.builders.builderDTO.ScheduleDTOBuilder;
+import com.app.dojo.builders.builderModels.ScheduleBuilder;
 import com.app.dojo.dtos.RoomDTO;
 import com.app.dojo.dtos.ScheduleDTO;
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -18,5 +21,23 @@ class SheduleControllerTest {
   @Autowired
   private TestRestTemplate testRestTemplate;
   private ScheduleDTO scheduleDTO;
-  private String url="http://localhost:8080/api/dojo-app/rooms";
+  private String url="http://localhost:8080/api/dojo-app/schedules";
+
+  @BeforeEach
+  void init(){
+    this.scheduleDTO= new ScheduleDTOBuilder()
+        .setDayName("Martes")
+        .setHoursClass("10:00-12:00")
+        .build();
+  }
+
+  @DisplayName("Test Schedule Controller, create a schedule")
+  @Test
+  @Order(1)
+  void create()throws Exception {
+    ResponseEntity<ScheduleDTO> response=testRestTemplate.postForEntity(url,scheduleDTO,ScheduleDTO.class);
+    assertEquals(201,response.getStatusCodeValue());
+    assertEquals(MediaType.APPLICATION_JSON,response.getHeaders().getContentType());
+    assertNotNull(response.getBody());
+  }
 }
