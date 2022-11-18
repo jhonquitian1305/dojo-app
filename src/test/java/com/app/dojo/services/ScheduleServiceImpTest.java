b96783ab2899d8502cf7766c283c0c51c1380a45
Andrés Castro <andres.cmonsalve@gmail.com>
2022-11-18 09:44:23 -0500

    Feat: Test to create a schedule in ScheduleServiceImpTest class}

--- src/test/java/com/app/dojo/services/ScheduleServiceImpTest.java
+++ src/test/java/com/app/dojo/services/ScheduleServiceImpTest.java
@@ -1,5 +1,6 @@
 package com.app.dojo.services;
 
+import com.app.dojo.builders.builderDTO.ScheduleDTOBuilder;
 import com.app.dojo.builders.builderModels.ScheduleBuilder;
 import com.app.dojo.dtos.ScheduleDTO;
 import com.app.dojo.mappers.MapperSchedule;
@@ -7,15 +8,23 @@ import com.app.dojo.models.Schedule;
 import com.app.dojo.repositories.ScheduleRepository;
 import com.app.dojo.services.implementation.ScheduleServiceImp;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 
 @ExtendWith(MockitoExtension.class)
+@ActiveProfiles(profiles = "test")
 class ScheduleServiceImpTest {
 
   @InjectMocks
@@ -26,5 +35,37 @@ class ScheduleServiceImpTest {
   private MapperSchedule mapperSchedule;
   private ScheduleDTO scheduleDTO;
   private Schedule schedule;
+  @BeforeEach
+  void init(){
+    schedule = new ScheduleBuilder()
+        .setId(1L)
+        .setDayName("Lunes")
+        .setHoursClass("8:00-10:00")
+        .build();
+
+    scheduleDTO = new ScheduleDTOBuilder()
+        .setId(1L)
+        .setDayName("Lunes")
+        .setHoursClass("8:00-10:00")
+        .build();
+  }
+
+  @DisplayName("Schedule Service test to create a schedule")
+  @Test()
+  void create(){
+    //given
+    given(this.scheduleRepository.save(any(Schedule.class))).willReturn(schedule);
+    given(this.scheduleRepository.findByDayNameAndHoursClass(scheduleDTO.getDayName(),scheduleDTO.getHoursClass())).willReturn(Optional.empty());
+
+    given(this.mapperSchedule.mapperScheduleDTO(any(Schedule.class))).willReturn(scheduleDTO);
+    given(this.mapperSchedule.mapperSchedule(any(ScheduleDTO.class))).willReturn(schedule);
+    //when
+    ScheduleDTO scheduleCreated=this.scheduleService.save(scheduleDTO);
+    //then
+    assertNotNull(scheduleCreated);
+    assertEquals(1L,scheduleCreated.getId());
+    assertEquals("Lunes",scheduleCreated.getDayName());
+    assertEquals("8:00-10:00",scheduleCreated.getHoursClass());
+  }
 
 }
