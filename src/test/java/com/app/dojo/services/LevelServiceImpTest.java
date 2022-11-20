6981f159a18206be0fe4aa322496693fc4f1d6dd
Andrés Castro <andres.cmonsalve@gmail.com>
2022-11-19 22:50:59 -0500

    Feat: Test to create a level in LevelServiceImpTest class

--- src/test/java/com/app/dojo/services/LevelServiceImpTest.java
+++ src/test/java/com/app/dojo/services/LevelServiceImpTest.java
@@ -6,20 +6,29 @@ import com.app.dojo.dtos.LevelDTO;
 import com.app.dojo.mappers.MapperLevel;
 import com.app.dojo.models.Level;
 import com.app.dojo.repositories.LevelRepository;
+import com.app.dojo.services.implementation.LevelServiceImp;
 import com.app.dojo.services.implementation.ScheduleServiceImp;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.context.ActiveProfiles;
 
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.assertj.core.api.Assertions.assertThat;
+
 @ExtendWith(MockitoExtension.class)
 @ActiveProfiles(profiles = "test")
 class LevelServiceImpTest {
     @InjectMocks
-    private ScheduleServiceImp scheduleServiceImp;
+    private LevelServiceImp levelService;
     @Mock
     private LevelRepository levelRepository;
     @Mock
@@ -37,5 +46,22 @@ class LevelServiceImpTest {
                 .setId(1L)
                 .setName("CINTA BLANCA")
                 .build();
+    }
+
+    @Test
+    @DisplayName("Test LevelService, create a level")
+    void create(){
+        //given
+        given(this.levelRepository.findByName(any(String.class))).willReturn(Optional.empty());
+        given(this.levelRepository.save(any(Level.class))).willReturn(level);
+
+        given(this.mapperLevel.mapperLevel(any(LevelDTO.class))).willReturn(level);
+        given(this.mapperLevel.mapperLevelDTO(any(Level.class))).willReturn(levelDTO);
+        //when
+        LevelDTO levelSaved=this.levelService.create(levelDTO);
+        //then
+        assertNotNull(levelSaved);
+        assertEquals(levelDTO.getName(),levelSaved.getName());
+        assertThat(levelSaved.getId()).isGreaterThan(0);
     }
 }
