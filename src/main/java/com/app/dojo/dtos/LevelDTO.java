fba0decda7edaba580b4e28def13aff2ad8a9dc5
Andrés Castro <andres.cmonsalve@gmail.com>
2022-11-19 20:16:08 -0500

    Feat: Adding messages to inform errors to user in LevelDTO class

--- src/main/java/com/app/dojo/dtos/LevelDTO.java
+++ src/main/java/com/app/dojo/dtos/LevelDTO.java
@@ -1,7 +1,15 @@
 package com.app.dojo.dtos;
 
+import com.app.dojo.constants.Message;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 public class LevelDTO {
   private Long id;
+  @NotNull(message =Message.FIELD_NULL)
+  @NotNull(message = Message.FIELD_EMPTY)
+  @Size(min = 5,message = Message.LEVEL_NAME_SIZE)
   private String name;
 
   public LevelDTO(Long id, String name) {
