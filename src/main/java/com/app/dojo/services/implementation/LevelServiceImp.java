956f9b404f16ae747706e85b9930a67cf28353c2
Andrés Castro <andres.cmonsalve@gmail.com>
2022-11-19 20:18:19 -0500

    Feat: Create a new Level

--- src/main/java/com/app/dojo/services/implementation/LevelServiceImp.java
+++ src/main/java/com/app/dojo/services/implementation/LevelServiceImp.java
@@ -1,12 +1,19 @@
 package com.app.dojo.services.implementation;
 
+import com.app.dojo.constants.Message;
 import com.app.dojo.dtos.LevelDTO;
 import com.app.dojo.dtos.LevelResponse;
+import com.app.dojo.exception.errors.BadRequest;
 import com.app.dojo.mappers.MapperLevel;
+import com.app.dojo.models.Level;
 import com.app.dojo.repositories.LevelRepository;
 import com.app.dojo.services.Interfaces.LevelService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+
+import java.util.Optional;
 
 @Service
 public class LevelServiceImp implements LevelService {
@@ -18,7 +25,11 @@ public class LevelServiceImp implements LevelService {
 
     @Override
     public LevelDTO create(LevelDTO levelDTO) {
-        return null;
+        Optional<Level> levelFound=this.levelRepository.findByName(levelDTO.getName().toUpperCase());
+        if(levelFound.isPresent()) throw new BadRequest(Message.MESSAGE_BAD_REQUEST_CREATE_LEVEL.formatted(levelDTO.getName()));
+        levelDTO.setName(levelDTO.getName().toUpperCase());
+        Level levelSaved=this.levelRepository.save(mapperLevel.mapperLevel(levelDTO));
+        return mapperLevel.mapperLevelDTO(levelSaved);
     }
 
     @Override
