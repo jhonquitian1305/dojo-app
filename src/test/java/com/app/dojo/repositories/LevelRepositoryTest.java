ceff9ad072bb40b05166e7137cb62a2e094441d7
Andrés Castro <andres.cmonsalve@gmail.com>
2022-11-19 21:56:42 -0500

    Feat: Test to create a level in LevelRepositoryTest class

--- src/test/java/com/app/dojo/repositories/LevelRepositoryTest.java
+++ src/test/java/com/app/dojo/repositories/LevelRepositoryTest.java
@@ -3,12 +3,15 @@ package com.app.dojo.repositories;
 import com.app.dojo.builders.builderModels.LevelBuilder;
 import com.app.dojo.models.Level;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Profile;
 import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
 @ActiveProfiles(profiles = "test")
@@ -24,6 +27,15 @@ class LevelRepositoryTest {
                 .setId(1L)
                 .setName("CINTA BLANCA")
                 .build();
+    }
+
+    @Test
+    @DisplayName("Test Repository Level, test to create a level")
+    void create(){
+        Level levelSaved=this.levelRepository.save(level);
+
+        assertNotNull(levelSaved);
+        assertThat(levelSaved.getId()).isGreaterThan(0);
     }
 
 }
