e931776eff44c1c125e3a20b383caba12564fad7
Andrés Castro <andres.cmonsalve@gmail.com>
2022-11-19 20:14:48 -0500

    Feat: Adding messages to inform errors to user

--- src/main/java/com/app/dojo/constants/Message.java
+++ src/main/java/com/app/dojo/constants/Message.java
@@ -2,6 +2,10 @@ package com.app.dojo.constants;
 
 public final class Message {
 
+
+    public static final String FIELD_NULL="El campo no puede ser nulo";
+    public static final String FIELD_EMPTY="El campo  no puede ser vacío";
+
     // Room Service
     public static final String MESSAGE_BAD_REQUEST_CREATE_ROOM="Already exists one room with that name";
     public static final String MESSAGE_NOT_FOUND_ROOM_ID="Doesn't exists a room with that id  %s";
@@ -13,4 +17,7 @@ public final class Message {
 
     //Level Service
     public static  final String MESSAGE_BAD_REQUEST_CREATE_LEVEL="There is already a saved level with that name: %s";
+
+    //Level
+    public static final String LEVEL_NAME_SIZE="El nombre del nivel debe ser mínimo de 5 letras";
 }
