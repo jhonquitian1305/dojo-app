053be8b63d6345e47b50ada2df127471dee57642
Andrés Castro <andres.cmonsalve@gmail.com>
2022-11-21 13:39:32 -0500

    Feat: Error message to inform the user about the level assigned to the course

--- src/main/java/com/app/dojo/constants/Message.java
+++ src/main/java/com/app/dojo/constants/Message.java
@@ -27,4 +27,5 @@ public final class Message {
 
     //Courses
     public static final String COURSES_PRICE="El precio del curso debe ser mayor a 0";
+    public static final String COURSES_LEVEL="El curo debe tener asignado un sólo nivel";
 }
