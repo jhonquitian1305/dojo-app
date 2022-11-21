6c31f3de9c82faff9e851db2f1ca83f5cf1258de
Andrés Castro <andres.cmonsalve@gmail.com>
2022-11-21 09:07:56 -0500

    Feat: Test to create a Level in LevelControllerTest class

--- src/test/java/com/app/dojo/controllers/LevelControllerTest.java
+++ src/test/java/com/app/dojo/controllers/LevelControllerTest.java
@@ -2,12 +2,13 @@ package com.app.dojo.controllers;
 
 import com.app.dojo.builders.builderDTO.LevelDTOBuilder;
 import com.app.dojo.dtos.LevelDTO;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -27,5 +28,16 @@ class LevelControllerTest {
                 .setId(1L)
                 .setName("Cinta Negra")
                 .build();
+    }
+
+    @Order(1)
+    @Test
+    @DisplayName("Test LevelController, create a level")
+    void create(){
+        ResponseEntity<LevelDTO> response=this.testRestTemplate.postForEntity(url,levelDTO,LevelDTO.class);
+        assertEquals(201,response.getStatusCodeValue());
+        assertEquals(HttpStatus.CREATED,response.getStatusCode());
+        assertNotNull(response.getBody());
+        assertEquals(MediaType.APPLICATION_JSON,response.getHeaders().getContentType());
     }
 }
