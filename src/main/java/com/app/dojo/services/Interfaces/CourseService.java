39dd5b631551e758b8be68cbb2368c040227a6c7
Andrés Castro <andres.cmonsalve@gmail.com>
2022-11-22 15:35:38 -0500

    Feat:Defining the method in the CourseService interface to search courses by level

--- src/main/java/com/app/dojo/services/Interfaces/CourseService.java
+++ src/main/java/com/app/dojo/services/Interfaces/CourseService.java
@@ -4,10 +4,13 @@ import com.app.dojo.dtos.CourseDTO;
 import com.app.dojo.dtos.CourseResponse;
 import com.app.dojo.models.Course;
 
+import java.util.List;
+
 public interface CourseService {
     Course create(CourseDTO courseDTO) throws Exception;
     Course getOne(Long id);
     CourseResponse getAll(int numberPage, int pageSize);
     Course update(Long id, CourseDTO courseDTO);
+    CourseResponse findByLevel(Long idLevel,int numberPage, int pageSize );
     void delete(Long id);
 }
