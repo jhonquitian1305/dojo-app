ee9f63fc8f4a4740784e8da4173dd97e7f01fcdc
Andrés Castro <andres.cmonsalve@gmail.com>
2022-11-22 10:30:29 -0500

    Feat: Adding error messages to inform users

--- src/main/java/com/app/dojo/constants/Message.java
+++ src/main/java/com/app/dojo/constants/Message.java
@@ -30,4 +30,9 @@ public final class Message {
     public static final String COURSES_LEVEL="El curo debe tener asignado un sólo nivel";
     public static final String COURSES_ROOM="El curso debe tener asignado mínimo un salón";
     public static final String COURSES_SCHEDULE="El curso debe tener asignado mínimo un horario";
+
+    //Course Service
+    public static final String MESSAGE_BAD_REQUEST_COURSES_DATE="The end date of the course must be after the start date.";
+    public static final String MESSAGE_BAD_REQUEST_COURSES_NAME="There is already a course saved under that name";
+    public static final String MESSAGE_BAD_REQUEST_COURSES_SIMILAR_SCHEDULE="There is already a course regsitrated with the level %s, in the classroom %s with class on %s with the following schedule %s";
 }
