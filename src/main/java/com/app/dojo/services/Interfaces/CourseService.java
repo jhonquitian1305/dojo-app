51416287be1f81557cf20bcda21df2618f9a53fb
Andrés Castro <andres.cmonsalve@gmail.com>
2022-11-21 21:20:44 -0500

    Feat: Creation of the CourseService interface with all the methods to be implemented

---
+++ src/main/java/com/app/dojo/services/Interfaces/CourseService.java
@@ -0,0 +1,14 @@
+package com.app.dojo.services.Interfaces;
+
+import com.app.dojo.dtos.CourseDTO;
+import com.app.dojo.dtos.CourseResponse;
+import com.app.dojo.models.Course;
+
+public interface CourseService {
+    Course create(CourseDTO courseDTO);
+    Course getOne(Long id);
+    Course getByName(String name);
+    CourseResponse getAll();
+    Course update(Long id, CourseDTO courseDTO);
+    void delete(Long id);
+}
