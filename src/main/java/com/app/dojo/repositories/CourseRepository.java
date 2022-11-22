a385fccb2d79a44fb3850d693f0f3afcd880acce
Andrés Castro <andres.cmonsalve@gmail.com>
2022-11-21 21:21:29 -0500

    Feat: creation of the Repository to Course model

---
+++ src/main/java/com/app/dojo/repositories/CourseRepository.java
@@ -0,0 +1,16 @@
+package com.app.dojo.repositories;
+
+import com.app.dojo.models.Course;
+import com.app.dojo.models.Level;
+import com.app.dojo.models.Room;
+import com.app.dojo.models.Schedule;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CourseRepository extends JpaRepository<Course,Long> {
+    Optional<Course> findByName(String name);
+    boolean existsCourseByLevelsAndRoomsAndSchedules(Level level, Room room, Schedule schedule);
+}
