288c375d13e1e22763ef687e535f1abcb7d37c9a
Andrés Castro <andres.cmonsalve@gmail.com>
2022-11-22 15:36:50 -0500

    Feat:Creating the method to search courses by level in the Course repository

--- src/main/java/com/app/dojo/repositories/CourseRepository.java
+++ src/main/java/com/app/dojo/repositories/CourseRepository.java
@@ -4,6 +4,8 @@ import com.app.dojo.models.Course;
 import com.app.dojo.models.Level;
 import com.app.dojo.models.Room;
 import com.app.dojo.models.Schedule;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -12,4 +14,5 @@ import java.util.Optional;
 @Repository
 public interface CourseRepository extends JpaRepository<Course,Long> {
     boolean existsCourseByName(String name);
+    Page<Course> findByLevel(Level level, Pageable pageable);
 }
