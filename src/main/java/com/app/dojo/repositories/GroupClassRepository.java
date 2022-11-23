6a8b9c69f21a8fe3124f77f7cb390b7fd19cf09a
Andrés Castro <andres.cmonsalve@gmail.com>
2022-11-22 20:26:50 -0500

    Feat: Adding the Repository annotation and extending the JpaRepository interface

--- src/main/java/com/app/dojo/repositories/GroupClassRepository.java
+++ src/main/java/com/app/dojo/repositories/GroupClassRepository.java
@@ -1,4 +1,9 @@
 package com.app.dojo.repositories;
 
-public interface GroupClassRepository {
+import com.app.dojo.models.GroupClass;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GroupClassRepository extends JpaRepository<GroupClass,Long> {
 }
