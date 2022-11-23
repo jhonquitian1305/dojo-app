11ab1a70b81c34c2c22a01a061a956aad652bf78
Andrés Castro <andres.cmonsalve@gmail.com>
2022-11-22 20:35:17 -0500

    Feat: Create GroupClassService interface with all methods to be implemented

---
+++ src/main/java/com/app/dojo/services/Interfaces/GroupClassService.java
@@ -0,0 +1,17 @@
+package com.app.dojo.services.Interfaces;
+
+import com.app.dojo.dtos.GroupClassDTO;
+import com.app.dojo.dtos.GroupClassResponse;
+import com.app.dojo.models.GroupClass;
+
+public interface GroupClassService {
+    GroupClass create(GroupClassDTO groupClassDTO);
+    GroupClass getOne(Long id);
+    GroupClassResponse getAll(int numberPage, int pageSize);
+    GroupClassResponse getByCourse(Long idCourse, int numberPage, int pageSize);
+    GroupClassResponse getByRoom(Long idRoom,int numberPage, int pageSize);
+    GroupClassResponse getBySchedule(Long idSchedule, int numberPage, int pageSize);
+    GroupClass update(Long id, GroupClassDTO groupClassDTO);
+    void delete(Long id);
+
+}
