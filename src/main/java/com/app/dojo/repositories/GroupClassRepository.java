85ecd9c2911e9dad819b2e5a8ec219147288f280
Andrés Castro <andres.cmonsalve@gmail.com>
2022-11-23 17:53:24 -0500

    Feat:Add methods to check if a GroupClass exists for some conditions

--- src/main/java/com/app/dojo/repositories/GroupClassRepository.java
+++ src/main/java/com/app/dojo/repositories/GroupClassRepository.java
@@ -1,9 +1,14 @@
 package com.app.dojo.repositories;
 
+import com.app.dojo.models.Course;
 import com.app.dojo.models.GroupClass;
+import com.app.dojo.models.Room;
+import com.app.dojo.models.Schedule;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface GroupClassRepository extends JpaRepository<GroupClass,Long> {
+    boolean existsGroupClassByNameClass(String name);
+    boolean existsGroupClassByRoomsAndSchedules(Room room, Schedule schedule);
 }
