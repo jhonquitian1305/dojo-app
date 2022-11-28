2aba63d4212b7e41b468fab502acd65b1dadfeca
Andrés Castro <andres.cmonsalve@gmail.com>
2022-11-28 13:33:31 -0500

    Feat: Creating  a method to update  GroupClass model information

--- src/main/java/com/app/dojo/mappers/MapperGroupClass.java
+++ src/main/java/com/app/dojo/mappers/MapperGroupClass.java
@@ -1,9 +1,15 @@
 package com.app.dojo.mappers;
 
 import com.app.dojo.builders.builderDTO.GroupClassDTOResponseBuilder;
+import com.app.dojo.dtos.GroupClassDTO;
 import com.app.dojo.dtos.GroupClassDTOResponse;
+import com.app.dojo.models.Course;
 import com.app.dojo.models.GroupClass;
+import com.app.dojo.models.Room;
+import com.app.dojo.models.Schedule;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class MapperGroupClass {
@@ -20,5 +26,17 @@ public class MapperGroupClass {
                 .setRooms(groupClass.getRooms())
                 .setSchedules(groupClass.getSchedules())
                 .build();
+    }
+
+    public GroupClass updateInformation(GroupClass groupClass, GroupClassDTO groupClassDTO, List<Schedule> schedules, List<Room> rooms, Course course){
+        groupClass.setNameClass(groupClassDTO.getNameClass());
+        groupClass.setCode(groupClassDTO.getCode());
+        groupClass.setHoursPerWeek(groupClassDTO.getHoursPerWeek());
+        groupClass.setTotalHours(groupClassDTO.getTotalHours());
+        groupClass.setWeeks(groupClass.getWeeks());
+        groupClass.setCourse(course);
+        groupClass.setSchedules(schedules);
+        groupClass.setRooms(rooms);
+        return  groupClass;
     }
 }
