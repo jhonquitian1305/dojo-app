62b1d3a41cd128138d7d92edf5c919c0295b2187
Andrés Castro <andres.cmonsalve@gmail.com>
2022-11-28 12:03:27 -0500

    Feat: Adding methods necessary methods to find groups by some conditions

--- src/main/java/com/app/dojo/repositories/GroupClassRepository.java
+++ src/main/java/com/app/dojo/repositories/GroupClassRepository.java
@@ -4,6 +4,8 @@ import com.app.dojo.models.Course;
 import com.app.dojo.models.GroupClass;
 import com.app.dojo.models.Room;
 import com.app.dojo.models.Schedule;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,4 +13,7 @@ import org.springframework.stereotype.Repository;
 public interface GroupClassRepository extends JpaRepository<GroupClass,Long> {
     boolean existsGroupClassByNameClass(String name);
     boolean existsGroupClassByRoomsAndSchedules(Room room, Schedule schedule);
+    Page<GroupClass> findByCourse(Course course, Pageable pageable);
+    Page<GroupClass> findByRooms(Room room, Pageable pageable);
+    Page<GroupClass> findBySchedules(Schedule schedule, Pageable pageable);
 }
