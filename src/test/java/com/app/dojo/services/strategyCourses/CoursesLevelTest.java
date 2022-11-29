7397532d794ad1a234b98134399fc004ce829c79
Andrés Castro <andres.cmonsalve@gmail.com>
2022-11-29 16:48:37 -0500

    Feat: Test to find courses by level

--- src/test/java/com/app/dojo/services/strategyCourses/CoursesLevelTest.java
+++ src/test/java/com/app/dojo/services/strategyCourses/CoursesLevelTest.java
@@ -7,16 +7,26 @@ import com.app.dojo.models.Level;
 import com.app.dojo.repositories.CourseRepository;
 import com.app.dojo.services.Interfaces.LevelService;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.BDDMockito.given;
 @ExtendWith(MockitoExtension.class)
 @ActiveProfiles(profiles = "test")
 class CoursesLevelTest {
@@ -45,6 +55,26 @@ class CoursesLevelTest {
         .setFinishDate(format.parse("2022-12-20"))
         .setId(1L)
         .build();
+  }
+
+  @Test
+  @DisplayName("Test CoursesLevelStrategy, Test to find courses by level")
+  void findCourses(){
+    //given
+    Pageable pageable= PageRequest.of(1,10);
+    Page<Course> courses= new PageImpl<>(List.of(course));
+    given(this.courseRepository.findByLevel(level,pageable)).willReturn(courses);
+    given(this.levelService.getOne(anyLong())).willReturn(level);
+    //when
+    Page<Course> coursesFound=this.coursesLevel.findCourses(pageable,anyLong());
+    //then
+    assertEquals(1,coursesFound.getTotalElements());
+    assertEquals(1,coursesFound.getSize());
+    assertEquals(1,coursesFound.getTotalPages());
+    assertEquals(0,coursesFound.getNumber());
+    assertTrue(coursesFound.isLast());
+    assertNotNull(coursesFound.getContent());
+    assertThat(coursesFound.getContent().size()).isGreaterThan(0);
   }
 
 }
