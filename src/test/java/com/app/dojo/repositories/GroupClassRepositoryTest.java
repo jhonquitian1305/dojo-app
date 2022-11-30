6d66b01343676e255a2018b6bb3526f151cdc6d0
Andrés Castro <andres.cmonsalve@gmail.com>
2022-11-30 13:50:39 -0500

    Feat: test to find groups by course

--- src/test/java/com/app/dojo/repositories/GroupClassRepositoryTest.java
+++ src/test/java/com/app/dojo/repositories/GroupClassRepositoryTest.java
@@ -8,6 +8,9 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.text.ParseException;
@@ -233,5 +236,24 @@ class GroupClassRepositoryTest {
     //then
     assertNotNull(groupsFound);
     assertThat(groupsFound.size()).isGreaterThan(0);
+  }
+
+  @Test
+  @DisplayName("Test GroupClassRepository, test to find groups by course")
+  void findByCourse(){
+    //given
+    Course courseSaved=this.courseRepository.save(course);
+    group.setCourse(courseSaved);
+    this.groupClassRepository.save(group);
+    //when
+    Pageable pageable= PageRequest.of(0,10);
+    Page<GroupClass> groups=this.groupClassRepository.findByCourse(course,pageable);
+    //then
+    assertAll(
+        ()->assertThat(groups.getContent().size()).isGreaterThan(0),
+        ()->assertEquals(1,groups.getTotalElements()),
+        ()->assertEquals(1,groups.getTotalPages()),
+        ()->assertTrue(groups.isLast())
+    );
   }
 }
