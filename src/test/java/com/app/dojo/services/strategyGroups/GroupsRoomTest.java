38b9ff768d4f3e9e977cb503d9e91aefb1c70f61
Andrés Castro <andres.cmonsalve@gmail.com>
2022-11-30 18:42:41 -0500

    Feat: Test to get all groups by room

--- src/test/java/com/app/dojo/services/strategyGroups/GroupsRoomTest.java
+++ src/test/java/com/app/dojo/services/strategyGroups/GroupsRoomTest.java
@@ -6,10 +6,16 @@ import com.app.dojo.repositories.GroupClassRepository;
 import com.app.dojo.services.Interfaces.RoomService;
 import com.app.dojo.services.Interfaces.ScheduleServcie;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.text.ParseException;
@@ -18,7 +24,11 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.BDDMockito.given;
+
 @ExtendWith(MockitoExtension.class)
 @ActiveProfiles(profiles = "test")
 class GroupsRoomTest {
@@ -79,5 +89,27 @@ class GroupsRoomTest {
         .setRooms(rooms)
         .setSchedules(schedules)
         .build();
+  }
+
+  @Test
+  @DisplayName("Test GroupsStrategy, Test to get all groups by room")
+  void findGroups() throws Exception {
+    //given
+    Page<GroupClass> groups=new PageImpl<>(List.of(group));
+    Pageable pageable= PageRequest.of(1,10);
+    given(this.groupClassRepository.findByRooms(room,pageable)).willReturn(groups);
+    given(this.roomService.findById(anyLong())).willReturn(room);
+    //when
+    Page<GroupClass> coursesFound=this.groups.findGroups(pageable,anyLong());
+    //then
+    assertAll(
+        ()->assertEquals(1,coursesFound.getTotalElements()),
+        ()->assertEquals(1,coursesFound.getSize()),
+        ()->assertEquals(1,coursesFound.getTotalPages()),
+        ()->assertEquals(0,coursesFound.getNumber()),
+        ()->assertTrue(coursesFound.isLast()),
+        ()->assertNotNull(coursesFound.getContent()),
+        ()->assertThat(coursesFound.getContent().size()).isGreaterThan(0)
+    );
   }
 }
