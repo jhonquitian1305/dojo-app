1a8157caaa71f00c43d16a9ad599b5a91756766f
Andrés Castro <andres.cmonsalve@gmail.com>
2022-11-30 18:04:37 -0500

    Feat: Test to load default strategy

--- src/test/java/com/app/dojo/services/strategyGroups/GroupsContextTest.java
+++ src/test/java/com/app/dojo/services/strategyGroups/GroupsContextTest.java
@@ -1,15 +1,26 @@
 package com.app.dojo.services.strategyGroups;
 
 import com.app.dojo.services.strategyCourses.CoursesContext;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyString;
+
 @ExtendWith(MockitoExtension.class)
 @ActiveProfiles(profiles = "test")
 class GroupsContextTest {
   @InjectMocks
-  private CoursesContext coursesContext;
+  private GroupsContext groupsContext;
+
+  @Test
+  @DisplayName("Test GroupsContext, Test to load default strategy")
+  void loadDefaultStrategy(){
+    GroupsStrategy defaultStrategy=this.groupsContext.loadStrategy("DEFAULT");
+    assertNotNull(defaultStrategy);
+  }
 }
