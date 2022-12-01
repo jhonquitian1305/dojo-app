1b8dc7c8b03b73c9ab8db9115eab2c6000eaa554
Andrés Castro <andres.cmonsalve@gmail.com>
2022-11-30 22:25:31 -0500

    Feat: Test to create a new group

--- src/test/java/com/app/dojo/controllers/GroupClassControllerTest.java
+++ src/test/java/com/app/dojo/controllers/GroupClassControllerTest.java
@@ -1,28 +1,24 @@
 package com.app.dojo.controllers;
 
-import com.app.dojo.builders.builderDTO.CourseDTOBuilder;
 import com.app.dojo.builders.builderDTO.GroupClassDTOBuilder;
-import com.app.dojo.builders.builderDTO.RoomDTOBuilder;
-import com.app.dojo.builders.builderDTO.ScheduleDTOBuilder;
-import com.app.dojo.builders.builderModels.*;
 import com.app.dojo.dtos.*;
-import com.app.dojo.models.Schedule;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
-@ActiveProfiles(profiles = "test")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class GroupClassControllerTest {
 
   @Autowired
@@ -30,7 +26,7 @@ class GroupClassControllerTest {
   private String url ="http://localhost:8080/api/dojo-app/groups";
   private GroupClassDTO groupClassDTO;
   @BeforeEach()
-  void init() throws ParseException {
+  void init() {
     groupClassDTO= new GroupClassDTOBuilder()
         .setCode("2345678")
         .setNameClass("PRINCIPIANTES 01")
@@ -41,5 +37,15 @@ class GroupClassControllerTest {
         .setSchedules(List.of(1L))
         .setRooms(List.of(1L))
         .build();
+  }
+  @Order(1)
+  @Test
+  @DisplayName("Test CourseController, create a course")
+  void create(){
+    ResponseEntity<GroupClassDTOResponse> response=this.testRestTemplate.postForEntity(url,groupClassDTO,GroupClassDTOResponse.class);
+    assertEquals(201,response.getStatusCodeValue());
+    assertEquals(HttpStatus.CREATED,response.getStatusCode());
+    assertNotNull(response.getBody());
+    assertEquals(MediaType.APPLICATION_JSON,response.getHeaders().getContentType());
   }
 }
