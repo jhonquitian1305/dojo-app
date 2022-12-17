9c5d573d56b1850b3ff8678a72f61d2f0d620923
Jhon Quitian <jhon.quitian@udea.edu.co>
2022-12-16 21:43:31 -0500

    Feat: Map Teacher Model to TeacherDTO

--- src/main/java/com/app/dojo/mappers/MapperTeacher.java
+++ src/main/java/com/app/dojo/mappers/MapperTeacher.java
@@ -1,5 +1,6 @@
 package com.app.dojo.mappers;
 
+import com.app.dojo.builders.builderDTO.TeacherDTOBuilder;
 import com.app.dojo.builders.builderModels.TeacherBuilder;
 import com.app.dojo.dtos.TeacherDTO;
 import com.app.dojo.models.Teacher;
@@ -14,6 +15,17 @@ public class MapperTeacher {
                 .setBirthday(teacherDTO.getBirthday())
                 .setEmail(teacherDTO.getEmail())
                 .setPassword(teacherDTO.getPassword())
+                .build();
+    }
+
+    public TeacherDTO mapTeacherDTO(Teacher teacher){
+        return new TeacherDTOBuilder()
+                .setId(teacher.getId())
+                .setDni(teacher.getDni())
+                .setNames(teacher.getNames())
+                .setLastnames(teacher.getLastnames())
+                .setEmail(teacher.getEmail())
+                .setPassword(teacher.getPassword())
                 .build();
     }
 }
