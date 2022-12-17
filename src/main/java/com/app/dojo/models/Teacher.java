d1cd41bf0160418a2d80a10669df64b16a3fdf85
Jhon Quitian <jhon.quitian@udea.edu.co>
2022-12-16 21:21:36 -0500

    Feat: Add Teacher Model

---
+++ src/main/java/com/app/dojo/models/Teacher.java
@@ -0,0 +1,16 @@
+package com.app.dojo.models;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.io.Serializable;
+import java.util.Date;
+@Entity
+@Table(name = "teachers")
+public class Teacher extends User implements Serializable {
+    public Teacher() {
+    }
+
+    public Teacher(Long id, String dni, String names, String lastnames, Date birthday, String email, String password) {
+        super(id, dni, names, lastnames, birthday, email, password);
+    }
+}
