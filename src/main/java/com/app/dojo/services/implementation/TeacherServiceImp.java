733cc1572523972ef58d5406cc1b539ddb8c070b
Jhon Quitian <jhon.quitian@udea.edu.co>
2022-12-18 09:04:21 -0500

    init: create implementation TeacherServiceImp

---
+++ src/main/java/com/app/dojo/services/implementation/TeacherServiceImp.java
@@ -0,0 +1,8 @@
+package com.app.dojo.services.implementation;
+
+import com.app.dojo.services.Interfaces.TeacherService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TeacherServiceImp implements TeacherService {
+}
