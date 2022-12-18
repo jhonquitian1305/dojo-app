caaa8e49e6aff2b81efad7e92aed4a3f1b95054d
Jhon Quitian <jhon.quitian@udea.edu.co>
2022-12-18 09:17:44 -0500

    Feat: find a teacher by its dni

--- src/main/java/com/app/dojo/services/implementation/TeacherServiceImp.java
+++ src/main/java/com/app/dojo/services/implementation/TeacherServiceImp.java
@@ -1,8 +1,22 @@
 package com.app.dojo.services.implementation;
 
+import com.app.dojo.exception.errors.BadRequest;
+import com.app.dojo.models.Teacher;
+import com.app.dojo.repositories.TeacherRepository;
 import com.app.dojo.services.Interfaces.TeacherService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TeacherServiceImp implements TeacherService {
+
+    @Autowired
+    TeacherRepository teacherRepository;
+
+    public void findTeacherByDni(String dni){
+        Teacher teacherFound = this.teacherRepository.findTeacherByDni(dni);
+        if(teacherFound != null){
+            throw new BadRequest("This teacher already exists");
+        }
+    }
 }
