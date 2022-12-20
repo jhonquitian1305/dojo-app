9b147fbf8f94e413eb5b01fb83618b626a063c5a
Jhon Quitian <jhon.quitian@udea.edu.co>
2022-12-20 10:48:36 -0500

    Test: TestRepository to save a teacher

--- src/test/java/com/app/dojo/repositories/TeacherRepositoryTest.java
+++ src/test/java/com/app/dojo/repositories/TeacherRepositoryTest.java
@@ -3,6 +3,8 @@ package com.app.dojo.repositories;
 import com.app.dojo.builders.builderModels.TeacherBuilder;
 import com.app.dojo.models.Teacher;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
@@ -10,6 +12,9 @@ import org.springframework.test.context.ActiveProfiles;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @DataJpaTest
 @ActiveProfiles(profiles = "test")
@@ -38,5 +43,14 @@ public class TeacherRepositoryTest {
                 .setEmail("jorgeortiz@mail.com")
                 .setPassword("987654321")
                 .build();
+    }
+
+    @DisplayName("Test Repository to save a teacher")
+    @Test
+    void save(){
+        Teacher teacherSaved = this.teacherRepository.save(teacher);
+
+        assertNotNull(teacherSaved);
+        assertThat(teacherSaved.getId()).isGreaterThan(0);
     }
 }
