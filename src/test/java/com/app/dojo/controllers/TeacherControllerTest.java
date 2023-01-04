370680b0faebec349e6abd8e349e4bc2e1fada6e
Jhon Quitian <jhon.quitian@udea.edu.co>
2023-01-03 20:38:08 -0500

    Test: init TeacherControllerTest

---
+++ src/test/java/com/app/dojo/controllers/TeacherControllerTest.java
@@ -0,0 +1,54 @@
+package com.app.dojo.controllers;
+
+import com.app.dojo.builders.builderDTO.TeacherDTOBuilder;
+import com.app.dojo.dtos.TeacherDTO;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles(profiles = "test")
+public class TeacherControllerTest {
+
+    @Autowired
+    private TestRestTemplate testRestTemplate;
+
+    private TeacherDTO teacherDTO;
+
+    private String url = "http://localhost:8080/api/dojo-app/teachers";
+
+    //Formatted date
+    String date = "23/11/2015";
+    SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+    Date formatDate = format.parse(date);
+
+
+    public TeacherControllerTest() throws ParseException {
+    }
+
+    @BeforeEach
+    void init(){
+        teacherDTO = new TeacherDTOBuilder()
+                .setId(1L)
+                .setDni("987654321")
+                .setNames("Jorge")
+                .setLastnames("Ortíz")
+                .setBirthday(formatDate)
+                .setEmail("jorgeortiz@mail.com")
+                .setPassword("987654321")
+                .build();
+    }
+}
