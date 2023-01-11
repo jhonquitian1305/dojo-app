f984b05f51308c9e3e2ec67387f43252cfd9f722
Jhon Quitian <jhon.quitian@udea.edu.co>
2023-01-11 16:16:18 -0500

    Feat: Add attemptAuthentication method

---
+++ src/main/java/com/app/dojo/security/JWTAuthenticationFilter.java
@@ -0,0 +1,36 @@
+package com.app.dojo.security;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.io.IOException;
+import java.util.Collections;
+
+public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+
+        AuthCredentials authCredentials = new AuthCredentials();
+
+        try {
+            authCredentials = new ObjectMapper().readValue(request.getReader(), AuthCredentials.class);
+        } catch (IOException e){
+        }
+
+        UsernamePasswordAuthenticationToken usernamePAT = new UsernamePasswordAuthenticationToken(
+                authCredentials.getEmail(),
+                authCredentials.getPassword(),
+                Collections.emptyList()
+        );
+        return getAuthenticationManager().authenticate(usernamePAT);
+    }
+}
