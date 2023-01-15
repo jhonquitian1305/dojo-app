830ff5a0a3e2cbb398fd0b4038f979822887fc47
Jhon Quitian <jhon.quitian@udea.edu.co>
2023-01-15 15:37:26 -0500

    init: create controller CommentController with its endpoint

---
+++ src/main/java/com/app/dojo/controllers/CommentController.java
@@ -0,0 +1,11 @@
+package com.app.dojo.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static com.app.dojo.constants.EndPointsConstants.ENDPOINT_COMMENTS;
+
+@RestController
+@RequestMapping(ENDPOINT_COMMENTS)
+public class CommentController {
+}
