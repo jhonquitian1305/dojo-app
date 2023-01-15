20534bc9648001b80b450022b5c2b71a7ed5de91
Jhon Quitian <jhon.quitian@udea.edu.co>
2023-01-15 15:34:43 -0500

    init: create implementation CommentServiceImp

---
+++ src/main/java/com/app/dojo/services/implementation/CommentServiceImp.java
@@ -0,0 +1,8 @@
+package com.app.dojo.services.implementation;
+
+import com.app.dojo.services.Interfaces.CommentService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CommentServiceImp implements CommentService {
+}
