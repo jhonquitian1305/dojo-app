b7d0b30437aed1de5cb4c41ee1006a51dcfce766
Jhon Quitian <jhon.quitian@udea.edu.co>
2023-01-17 15:18:47 -0500

    Feat: method that allow find comments by course

--- src/main/java/com/app/dojo/repositories/CommentRepository.java
+++ src/main/java/com/app/dojo/repositories/CommentRepository.java
@@ -1,9 +1,14 @@
 package com.app.dojo.repositories;
 
 import com.app.dojo.models.Comment;
+import com.app.dojo.models.Course;
+import com.app.dojo.models.Teacher;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+    Page<Comment> findByCourse(Course course, Pageable pageable);
 }
