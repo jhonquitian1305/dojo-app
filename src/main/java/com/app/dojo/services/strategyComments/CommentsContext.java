02868fd5340a0dbac5b27bae7981f5e2b15fbc4f
Jhon Quitian <jhon.quitian@udea.edu.co>
2023-01-17 15:41:43 -0500

    Feat: load strategy by teacher

--- src/main/java/com/app/dojo/services/strategyComments/CommentsContext.java
+++ src/main/java/com/app/dojo/services/strategyComments/CommentsContext.java
@@ -2,6 +2,7 @@ package com.app.dojo.services.strategyComments;
 
 import com.app.dojo.repositories.CommentRepository;
 import com.app.dojo.services.Interfaces.CourseService;
+import com.app.dojo.services.Interfaces.TeacherService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -16,10 +17,15 @@ public class CommentsContext {
     @Autowired
     private CommentRepository commentRepository;
 
+    @Autowired
+    private TeacherService teacherService;
+
     public CommentsStrategy loadStrategy(String modelCondition){
         switch (modelCondition){
             case "COURSE":
                 return new CommentsCourse(this.courseService, this.commentRepository);
+            case "TEACHER":
+                return new CommentsTeacher(this.teacherService, this.commentRepository);
             default:
                 return new Comments(this.commentRepository);
         }
