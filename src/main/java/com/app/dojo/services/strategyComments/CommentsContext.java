78b37f1c9f910840d1f45036f0d3cf8c2aad9c74
Jhon Quitian <jhon.quitian@udea.edu.co>
2023-01-17 13:30:33 -0500

    Feat: load strategy according model Comment(default)

--- src/main/java/com/app/dojo/services/strategyComments/CommentsContext.java
+++ src/main/java/com/app/dojo/services/strategyComments/CommentsContext.java
@@ -1,7 +1,25 @@
 package com.app.dojo.services.strategyComments;
 
+import com.app.dojo.repositories.CommentRepository;
+import com.app.dojo.services.Interfaces.CourseService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CommentsContext {
+
+    private CommentsStrategy commentsStrategy;
+
+    @Autowired
+    private CourseService courseService;
+
+    @Autowired
+    private CommentRepository commentRepository;
+
+    public CommentsStrategy loadStrategy(String modelCondition){
+        switch (modelCondition){
+            default:
+                return new Comments(this.commentRepository);
+        }
+    }
 }
