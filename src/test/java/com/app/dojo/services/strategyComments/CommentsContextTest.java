36c510096014351de96c1e64bd1567f10475d700
Jhon Quitian <jhon.quitian@udea.edu.co>
2023-01-18 16:46:12 -0500

    Test: CommentsContext to get default strategy to find comments

--- src/test/java/com/app/dojo/services/strategyComments/CommentsContextTest.java
+++ src/test/java/com/app/dojo/services/strategyComments/CommentsContextTest.java
@@ -1,13 +1,24 @@
 package com.app.dojo.services.strategyComments;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @ExtendWith(MockitoExtension.class)
 @ActiveProfiles(profiles = "test")
 public class CommentsContextTest {
     @InjectMocks
     private CommentsContext commentsContext;
+
+    @DisplayName("Test CommentsContext to get default strategy to find comments")
+    @Test
+    void loadDefaultStrategy(){
+        CommentsStrategy defaultStrategy = this.commentsContext.loadStrategy("DEFAULT");
+        assertNotNull(defaultStrategy);
+    }
 }
