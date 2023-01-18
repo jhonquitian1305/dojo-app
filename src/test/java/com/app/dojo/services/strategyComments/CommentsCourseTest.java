335fa22ec7294a4efba2b58af315f97366a48a7d
Jhon Quitian <jhon.quitian@udea.edu.co>
2023-01-18 17:26:36 -0500

    Test: create TestStrategyComments to get all comments by course

--- src/test/java/com/app/dojo/services/strategyComments/CommentsCourseTest.java
+++ src/test/java/com/app/dojo/services/strategyComments/CommentsCourseTest.java
@@ -11,16 +11,29 @@ import com.app.dojo.models.Teacher;
 import com.app.dojo.repositories.CommentRepository;
 import com.app.dojo.services.Interfaces.CourseService;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.util.Date;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.BDDMockito.given;
 
 @ExtendWith(MockitoExtension.class)
 @ActiveProfiles(profiles = "test")
@@ -79,5 +92,26 @@ public class CommentsCourseTest {
                 .setTeacher(teacher)
                 .setStudent(student)
                 .build();
+    }
+
+    @DisplayName("Test strategyComments to get all comments by course")
+    @Test
+    void findComments(){
+        Page<Comment> comments = new PageImpl<>(List.of(comment));
+        Pageable pageable = PageRequest.of(1, 10);
+        given(this.commentRepository.findByCourse(course, pageable)).willReturn(comments);
+        given(this.courseService.getOne(anyLong())).willReturn(course);
+
+        Page<Comment> commentsFound = this.comments.findComments(pageable, anyLong());
+
+        assertAll(
+                ()->assertEquals(1, commentsFound.getTotalElements()),
+                ()->assertEquals(1, commentsFound.getSize()),
+                ()->assertEquals(1, commentsFound.getTotalPages()),
+                ()->assertEquals(0, commentsFound.getNumber()),
+                ()->assertTrue(commentsFound.isLast()),
+                ()->assertNotNull(commentsFound.getContent()),
+                ()->assertThat(commentsFound.getContent().size()).isGreaterThan(0)
+        );
     }
 }
