abdd6bd7e21f964655624887edd4907542792783
Jhon Quitian <jhon.quitian@udea.edu.co>
2023-01-18 17:48:37 -0500

    init: create the CommentsTeacherTest class to implement the test of the CommentsTeacher class

---
+++ src/test/java/com/app/dojo/services/strategyComments/CommentsTeacherTest.java
@@ -0,0 +1,83 @@
+package com.app.dojo.services.strategyComments;
+
+import com.app.dojo.builders.builderModels.CommentBuilder;
+import com.app.dojo.builders.builderModels.CourseBuilder;
+import com.app.dojo.builders.builderModels.StudentBuilder;
+import com.app.dojo.builders.builderModels.TeacherBuilder;
+import com.app.dojo.models.Comment;
+import com.app.dojo.models.Course;
+import com.app.dojo.models.Student;
+import com.app.dojo.models.Teacher;
+import com.app.dojo.repositories.CommentRepository;
+import com.app.dojo.services.Interfaces.TeacherService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.Date;
+
+@ExtendWith(MockitoExtension.class)
+@ActiveProfiles(profiles = "test")
+public class CommentsTeacherTest {
+    @InjectMocks
+    private CommentsTeacher comments;
+    @Mock
+    private CommentRepository commentRepository;
+    @Mock
+    private TeacherService teacherService;
+
+    private Comment comment;
+    private Teacher teacher;
+
+    LocalDate localDateObject = LocalDate.now();
+
+    //Formatted date
+    String date = "04/02/1995";
+    SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+    Date formatDate = format.parse(date);
+    Date startDate = format.parse("01/06/2022");
+    Date finishDate = format.parse("30/06/2022");
+
+    public CommentsTeacherTest() throws ParseException {
+    }
+
+    @BeforeEach
+    void begin(){
+        Course course = new CourseBuilder()
+                .setId(1L)
+                .setName("CINTA NEGRA PRINCIPIANTES")
+                .setStartDate(startDate)
+                .setFinishDate(finishDate)
+                .setPrice(200000.0)
+                .build();
+        teacher = new TeacherBuilder()
+                .setDni("987654321")
+                .setNames("Jorge")
+                .setLastnames("Ortíz")
+                .setBirthday(formatDate)
+                .setEmail("jorgeortiz@mail.com")
+                .setPassword("987654321")
+                .build();
+        Student student = new StudentBuilder()
+                .setDni("12345678")
+                .setNames("Jhon")
+                .setLastnames("Quitian")
+                .setBirthday(formatDate)
+                .setEmail("jhonquitian@mail.com")
+                .setPassword("12345678")
+                .build();
+        comment = new CommentBuilder()
+                .setDateComment(localDateObject)
+                .setComment("Buen trabajo")
+                .setCourse(course)
+                .setTeacher(teacher)
+                .setStudent(student)
+                .build();
+    }
+}
