577bdb579751db55d3dbb42d988c0c42f78f1e3c
Jhon Quitian <jhon.quitian@udea.edu.co>
2023-01-19 13:02:59 -0500

    Test: create TestService to create a comment

--- src/test/java/com/app/dojo/services/CommentsServiceTest.java
+++ src/test/java/com/app/dojo/services/CommentsServiceTest.java
@@ -6,8 +6,13 @@ import com.app.dojo.dtos.CommentDTO;
 import com.app.dojo.mappers.MapperComment;
 import com.app.dojo.models.*;
 import com.app.dojo.repositories.CommentRepository;
+import com.app.dojo.services.Interfaces.CourseService;
+import com.app.dojo.services.Interfaces.StudentService;
+import com.app.dojo.services.Interfaces.TeacherService;
 import com.app.dojo.services.implementation.CommentServiceImp;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -18,6 +23,11 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.BDDMockito.given;
 
 @ExtendWith(MockitoExtension.class)
 @ActiveProfiles(profiles = "test")
@@ -31,6 +41,15 @@ public class CommentsServiceTest {
 
     @Mock
     private MapperComment mapperComment;
+
+    @Mock
+    private CourseService courseService;
+
+    @Mock
+    private TeacherService teacherService;
+
+    @Mock
+    private StudentService studentService;
 
     private Comment comment;
     private Course course;
@@ -95,5 +114,20 @@ public class CommentsServiceTest {
                 .setTeacher(1L)
                 .setStudent(1L)
                 .build();
+    }
+
+    @DisplayName("Test service to create a comment")
+    @Test
+    void create() throws Exception {
+        given(this.courseService.getOne(anyLong())).willReturn(course);
+        given(this.teacherService.getById(anyLong())).willReturn(teacher);
+        given(this.studentService.getStudentById(anyLong())).willReturn(student);
+        given(this.commentRepository.save(any(Comment.class))).willReturn(comment);
+
+        given(this.mapperComment.createComment(any(CommentDTO.class), any(Course.class), any(Teacher.class), any(Student.class))).willReturn(comment);
+
+        Comment commentSaved = this.commentServiceImp.createOne(commentDTO);
+
+        assertNotNull(commentSaved);
     }
 }
