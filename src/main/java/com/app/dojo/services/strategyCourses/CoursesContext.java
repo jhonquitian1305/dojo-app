416a4d595eb4b2280d4eca590e8e14548d259be5
Jhon Quitian <jhon.quitian@udea.edu.co>
2023-01-24 16:28:21 -0500

    Feat: load strategy by teacher

--- src/main/java/com/app/dojo/services/strategyCourses/CoursesContext.java
+++ src/main/java/com/app/dojo/services/strategyCourses/CoursesContext.java
@@ -2,6 +2,7 @@ package com.app.dojo.services.strategyCourses;
 
 import com.app.dojo.repositories.CourseRepository;
 import com.app.dojo.services.Interfaces.LevelService;
+import com.app.dojo.services.Interfaces.TeacherService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -12,11 +13,15 @@ public class CoursesContext {
   private LevelService levelService;
   @Autowired
   private CourseRepository courseRepository;
+  @Autowired
+  private TeacherService teacherService;
 
   public CoursesStrategy loadStrategy(String modelCondition){
     switch (modelCondition){
       case "LEVEL":
         return new CoursesLevel(levelService,courseRepository);
+      case "TEACHER":
+        return new CoursesTeacher(this.teacherService, courseRepository);
       default:
         return new Courses(courseRepository);
     }
