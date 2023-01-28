a0e2b01d56bfd6fd339c4bf31df3a7dead8cfdc0
Jhon Quitian <jhon.quitian@udea.edu.co>
2023-01-28 18:02:07 -0500

    init: create controller DiplomaController with its endpoint

---
+++ src/main/java/com/app/dojo/controllers/DiplomaController.java
@@ -0,0 +1,9 @@
+package com.app.dojo.controllers;
+import org.springframework.web.bind.annotation.*;
+
+import static com.app.dojo.constants.EndPointsConstants.*;
+
+@RestController
+@RequestMapping(ENDPOINT_DIPLOMAS)
+public class DiplomaController {
+}
