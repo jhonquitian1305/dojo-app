8385fee2082bcaff5d8200d9ec5af0ab7339de88
Jhon Quitian <jhon.quitian@udea.edu.co>
2023-01-28 17:57:10 -0500

    init: create interface DiplomaRepository

---
+++ src/main/java/com/app/dojo/repositories/DiplomaRepository.java
@@ -0,0 +1,9 @@
+package com.app.dojo.repositories;
+
+import com.app.dojo.models.Diploma;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DiplomaRepository extends JpaRepository<Diploma, Long> {
+}
