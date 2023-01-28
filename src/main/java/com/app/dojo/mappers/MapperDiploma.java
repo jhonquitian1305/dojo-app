1d343500bba7b7cc550a462f5dfa821295a5349c
Jhon Quitian <jhon.quitian@udea.edu.co>
2023-01-28 17:55:14 -0500

    Feat: Add createDiploma method

--- src/main/java/com/app/dojo/mappers/MapperDiploma.java
+++ src/main/java/com/app/dojo/mappers/MapperDiploma.java
@@ -1,8 +1,11 @@
 package com.app.dojo.mappers;
 
 import com.app.dojo.builders.builderDTO.DiplomaDTOResponseBuilder;
+import com.app.dojo.builders.builderModels.DiplomaBuilder;
+import com.app.dojo.dtos.DiplomaDTO;
 import com.app.dojo.dtos.DiplomaDTOResponse;
 import com.app.dojo.models.Diploma;
+import com.app.dojo.models.User;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -12,6 +15,14 @@ public class MapperDiploma {
                 .setId(diploma.getId())
                 .setDiplomaName(diploma.getDiplomaName())
                 .setUser(diploma.getUser())
+                .build();
+    }
+
+    public Diploma createDiploma(DiplomaDTO diplomaDTO, User user){
+        return new DiplomaBuilder()
+                .setId(diplomaDTO.getId())
+                .setDiplomaName(diplomaDTO.getDiplomaName())
+                .setUser(user)
                 .build();
     }
 }
