e12ce55c266d65ef2ae01ae1d7c74fca8acc2a51
Jhon Quitian <jhon.quitian@udea.edu.co>
2023-01-28 18:00:36 -0500

    init: create implementation DiplomaServiceImp

---
+++ src/main/java/com/app/dojo/services/implementation/DiplomaServiceImp.java
@@ -0,0 +1,7 @@
+package com.app.dojo.services.implementation;
+import com.app.dojo.services.Interfaces.DiplomaService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DiplomaServiceImp implements DiplomaService {
+}
