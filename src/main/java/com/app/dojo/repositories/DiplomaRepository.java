577f86b4fb3d00baa8283621dbcaeb7c0d29a8ce
Jhon Quitian <jhon.quitian@udea.edu.co>
2023-01-28 20:04:35 -0500

    Feat: method to find all diplomas of a student

--- src/main/java/com/app/dojo/repositories/DiplomaRepository.java
+++ src/main/java/com/app/dojo/repositories/DiplomaRepository.java
@@ -1,10 +1,14 @@
 package com.app.dojo.repositories;
 
 import com.app.dojo.models.Diploma;
+import com.app.dojo.models.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface DiplomaRepository extends JpaRepository<Diploma, Long> {
     Boolean existsByDiplomaName(String diplomaName);
+    Page<Diploma> findByUser(User user, Pageable pageable);
 }
