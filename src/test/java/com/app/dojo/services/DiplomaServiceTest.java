da3cd8aaa3973f6641e4416038b8797ac700683c
Jhon Quitian <jhon.quitian@udea.edu.co>
2023-01-30 15:50:41 -0500

    Test: create TestService to create a diploma for a teacher

--- src/test/java/com/app/dojo/services/DiplomaServiceTest.java
+++ src/test/java/com/app/dojo/services/DiplomaServiceTest.java
@@ -3,12 +3,15 @@ package com.app.dojo.services;
 import com.app.dojo.builders.builderDTO.DiplomaDTOBuilder;
 import com.app.dojo.builders.builderModels.DiplomaBuilder;
 import com.app.dojo.builders.builderModels.StudentBuilder;
+import com.app.dojo.builders.builderModels.TeacherBuilder;
 import com.app.dojo.dtos.DiplomaDTO;
 import com.app.dojo.mappers.MapperDiploma;
 import com.app.dojo.models.Diploma;
 import com.app.dojo.models.Student;
+import com.app.dojo.models.Teacher;
 import com.app.dojo.repositories.DiplomaRepository;
 import com.app.dojo.services.Interfaces.StudentService;
+import com.app.dojo.services.Interfaces.TeacherService;
 import com.app.dojo.services.implementation.DiplomaServiceImp;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -41,11 +44,17 @@ public class DiplomaServiceTest {
     private StudentService studentService;
 
     @Mock
+    private TeacherService teacherService;
+
+    @Mock
     private MapperDiploma mapperDiploma;
 
     private Diploma diplomaStudent;
+    private Diploma diplomaTeacher;
     private DiplomaDTO diplomaStudentDTO;
+    private DiplomaDTO diplomaTeacherDTO;
     private Student student;
+    private Teacher teacher;
 
     //Formatted date
     String date = "23/11/2015";
@@ -75,6 +84,25 @@ public class DiplomaServiceTest {
                 .setDiplomaName("Certificado cinturón verde")
                 .setUser(1L)
                 .build();
+
+        teacher = new TeacherBuilder()
+                .setId(2L)
+                .setDni("987654321")
+                .setNames("Jorge")
+                .setLastnames("Ortíz")
+                .setBirthday(formatDate)
+                .setEmail("jorgeortiz@mail.com")
+                .setPassword("987654321")
+                .build();
+        diplomaTeacher = new DiplomaBuilder()
+                .setId(1L)
+                .setDiplomaName("Certificado cinturón verde")
+                .setUser(teacher)
+                .build();
+        diplomaTeacherDTO = new DiplomaDTOBuilder()
+                .setDiplomaName("Certificado cinturón verde")
+                .setUser(2L)
+                .build();
     }
 
     @DisplayName("Test Service to create a diploma for a student")
@@ -86,6 +114,19 @@ public class DiplomaServiceTest {
         given(this.mapperDiploma.createDiploma(any(DiplomaDTO.class), any(Student.class))).willReturn(diplomaStudent);
 
         Diploma diplomaSaved = this.diplomaService.saveDiplomaStudent(anyLong(), diplomaStudentDTO);
+
+        assertNotNull(diplomaSaved);
+    }
+
+    @DisplayName("Test Service to create a diploma for a teacher")
+    @Test
+    void createDiplomaTeacher(){
+        given(this.diplomaRepository.existsByDiplomaName(anyString())).willReturn(false);
+        given(this.diplomaRepository.save(any(Diploma.class))).willReturn(diplomaTeacher);
+        given(this.teacherService.getById(anyLong())).willReturn(teacher);
+        given(this.mapperDiploma.createDiploma(any(DiplomaDTO.class), any(Teacher.class))).willReturn(diplomaTeacher);
+
+        Diploma diplomaSaved = this.diplomaService.saveDiplomaTeacher(anyLong(), diplomaTeacherDTO);
 
         assertNotNull(diplomaSaved);
     }
