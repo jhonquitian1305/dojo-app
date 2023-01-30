84715ab3fe447a1a45b698304f8202fa960ee5d0
Jhon Quitian <jhon.quitian@udea.edu.co>
2023-01-29 19:26:05 -0500

    Test: create TestRepository to save a diploma for a teacher

--- src/test/java/com/app/dojo/repositories/DiplomaRepositoryTest.java
+++ src/test/java/com/app/dojo/repositories/DiplomaRepositoryTest.java
@@ -2,8 +2,10 @@ package com.app.dojo.repositories;
 
 import com.app.dojo.builders.builderModels.DiplomaBuilder;
 import com.app.dojo.builders.builderModels.StudentBuilder;
+import com.app.dojo.builders.builderModels.TeacherBuilder;
 import com.app.dojo.models.Diploma;
 import com.app.dojo.models.Student;
+import com.app.dojo.models.Teacher;
 import com.app.dojo.models.User;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -29,8 +31,12 @@ public class DiplomaRepositoryTest {
     @Autowired
     StudentRepository studentRepository;
 
+    @Autowired
+    TeacherRepository teacherRepository;
+
     private Diploma diploma;
     private Student student;
+    private Teacher teacher;
 
     //Formatted date
     String date = "23/11/2015";
@@ -55,6 +61,15 @@ public class DiplomaRepositoryTest {
                 .setEmail("jhonquitian@mail.com")
                 .setPassword("12345678")
                 .build();
+        teacher = new TeacherBuilder()
+                .setId(2L)
+                .setDni("987654321")
+                .setNames("Jorge")
+                .setLastnames("Ortíz")
+                .setBirthday(formatDate)
+                .setEmail("jorgeortiz@mail.com")
+                .setPassword("987654321")
+                .build();
     }
 
     @DisplayName("Test Repository to create a diploma for a student")
@@ -63,6 +78,18 @@ public class DiplomaRepositoryTest {
         User studentSaved = this.studentRepository.save(student);
 
         diploma.setUser(studentSaved);
+        Diploma diplomaSaved = this.diplomaRepository.save(diploma);
+
+        assertNotNull(diplomaSaved);
+        assertThat(diplomaSaved.getId()).isGreaterThan(0);
+    }
+
+    @DisplayName("Test Repository to create a diploma for a teacher")
+    @Test
+    void createDiplomaTeacher(){
+        User teacherSaved = this.teacherRepository.save(teacher);
+
+        diploma.setUser(teacherSaved);
         Diploma diplomaSaved = this.diplomaRepository.save(diploma);
 
         assertNotNull(diplomaSaved);
