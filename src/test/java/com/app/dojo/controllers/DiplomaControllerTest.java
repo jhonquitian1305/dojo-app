94c5f71f563d64d39443463c2fc8e24b006c96b3
Jhon Quitian <jhon.quitian@udea.edu.co>
2023-01-30 20:17:28 -0500

    Test: create TestController to create a diploma for a student

--- src/test/java/com/app/dojo/controllers/DiplomaControllerTest.java
+++ src/test/java/com/app/dojo/controllers/DiplomaControllerTest.java
@@ -1,14 +1,27 @@
 package com.app.dojo.controllers;
 
 import com.app.dojo.builders.builderDTO.DiplomaDTOBuilder;
+import com.app.dojo.builders.builderDTO.StudentDTOBuilder;
+import com.app.dojo.builders.builderModels.StudentBuilder;
 import com.app.dojo.dtos.DiplomaDTO;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.TestMethodOrder;
+import com.app.dojo.dtos.DiplomaDTOResponse;
+import com.app.dojo.dtos.StudentDTO;
+import com.app.dojo.dtos.UserDTO;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -17,12 +30,46 @@ public class DiplomaControllerTest {
     @Autowired
     private TestRestTemplate testRestTemplate;
 
-    private DiplomaDTO diplomaDTO;
+    private String urlDiplomaStudent = "http://localhost:8080/api/dojo-app/diplomas/students/1";
+    private String urlStudent = "http://localhost:8080/api/dojo-app/students";
+
+    private DiplomaDTO diplomaStudentDTO;
+    private UserDTO studentDTO;
+
+    //Formatted date
+    String date = "23/11/2015";
+    SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+    Date formatDate = format.parse(date);
+
+    public DiplomaControllerTest() throws ParseException {
+    }
 
     @BeforeEach
     void begin(){
-        diplomaDTO = new DiplomaDTOBuilder()
+        studentDTO = new StudentDTOBuilder()
+                .setId(1L)
+                .setDni("12345678")
+                .setNames("Jhon")
+                .setLastnames("Quitian")
+                .setBirthday(formatDate)
+                .setEmail("jhonquitian@mail.com")
+                .setPassword("12345678")
+                .build();
+        diplomaStudentDTO = new DiplomaDTOBuilder()
                 .setDiplomaName("Certificado cinturón verde")
                 .build();
+    }
+
+    @DisplayName("Test controller to create a diploma for a student")
+    @Test
+    @Order(1)
+    void createDiplomaStudent(){
+        ResponseEntity<StudentDTO> studentSaved = this.testRestTemplate.postForEntity(urlStudent, studentDTO, StudentDTO.class);
+
+        ResponseEntity<DiplomaDTOResponse> response = this.testRestTemplate.postForEntity(urlDiplomaStudent, diplomaStudentDTO, DiplomaDTOResponse.class);
+
+        assertEquals(HttpStatus.CREATED,response.getStatusCode());
+        assertNotNull(response.getBody());
+        assertEquals(MediaType.APPLICATION_JSON,response.getHeaders().getContentType());
     }
 }
